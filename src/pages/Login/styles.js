848053f0322321bd764acb5531399841f2491571
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-01-11 17:22:36 -0300

    update in styles from home.

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  flex: 1;
+  background-color: #36393f;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Title = styled.Text``;
