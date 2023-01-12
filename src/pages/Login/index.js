5e2782806551d0fda30dba2ff724d7ec4ef5b3a2
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-01-12 17:22:25 -0300

    update

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,31 @@
-import {View, Text} from 'react-native';
+import {View, Text, Keyboard, TouchableWithoutFeedback} from 'react-native';
 import React from 'react';
-import {Container, Title, SubTitle, Input, Button, ButtonText} from './styles';
+import {
+  Container,
+  Title,
+  SubTitle,
+  Input,
+  Button,
+  ButtonText,
+  SignUpButton,
+  SignUpText,
+} from './styles';
 export default function Login() {
   return (
-    <Container>
-      <Title>
-        Dev<SubTitle>Post</SubTitle>
-      </Title>
-      <Input placeholder="teste@gmail.com" />
-      <Input placeholder="*********" />
-      <Button>
-        <ButtonText>Acessar</ButtonText>
-      </Button>
-    </Container>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Container onPress={() => Keyboard.dismiss()}>
+        <Title>
+          Dev<SubTitle>Post</SubTitle>
+        </Title>
+        <Input placeholder="teste@gmail.com" />
+        <Input placeholder="*********" />
+        <Button>
+          <ButtonText>Acessar</ButtonText>
+        </Button>
+        <SignUpButton>
+          <SignUpText>Criar uma conta</SignUpText>
+        </SignUpButton>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -8,12 +8,55 @@ export const Container = styled.View`
   align-items: center;
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+font-size: 50px;
+color: #ffffff;
+font-weight: bold;
+font-style: italic;
+`;
 
-export const SubTitle = styled.Text``;
+export const SubTitle = styled.Text`
+color:#e52246;
+`;
 
-export const Input = styled.TextInput``;
+export const Input = styled.TextInput`
+background-color: #ffffff;
+border-radius: 10px;
+margin-top: 10px;
+width: 80%;
+height: 45px;
+padding: 5px;
+padding-left: 15px;
+font-size: 16px;
+justify-content: center;
+`;
 
-export const Button = styled.TouchableOpacity``;
+export const Button = styled.TouchableOpacity`
+background-color: #428cfd;
+border-radius: 10px;
+margin-top: 10px;
+width: 80%;
+height: 45px;
+padding: 5px;
+font-size: 16px;
+align-items: center;
+justify-content: center;
+`;
 
-export const ButtonText = styled.Text``;
+export const ButtonText = styled.Text`
+color: #FFF;
+font-size: 22px;
+`;
+
+export const SignUpButton = styled.TouchableOpacity`
+width: 100%;
+margin-top: 10px;
+justify-content: center;
+align-items: center;
+`
+
+export const SignUpText = styled.Text`
+  color: #DDD;
+  font-size: 18px;
+  font-style: italic;
+`
